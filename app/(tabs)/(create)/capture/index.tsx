414014ef953eb95bafc82d3f8ffca820bc5c1c14
GSDV <sdvgab@gmail.com>
2025-03-22 19:07:01 -0500

    Removing safeareas to add inside of camera view

--- app/(tabs)/(create)/capture/index.tsx
+++ app/(tabs)/(create)/capture/index.tsx
@@ -1,13 +1,7 @@
 import CapturePicture from '@screens/post/Create/CapturePicture';
 
-import { SafeAreaFull } from '@components/SafeArea';
-
 
 
 export default function Index() {
-    return (
-        <SafeAreaFull>
-            <CapturePicture />
-        </SafeAreaFull>
-    );
+    return <CapturePicture />;
 }
