84db92d716bd064f4658224374fa1a40b3ed410d
GSDV <sdvgab@gmail.com>
2025-03-24 14:19:25 -0500

    Init brands

---
+++ src/util/web/brands.ts
@@ -0,0 +1,13 @@
+interface Brand {
+    name: string,
+    logoUrl: string,
+    url: string,
+}
+
+
+
+export const brands: Brand[] = [
+    { name: 'Zara', logoUrl: '', url: 'https://www.zara.com/us' },
+    { name: 'H&M', logoUrl: '', url: 'https://www2.hm.com' },
+    { name: 'Balenciaga', logoUrl: '', url: 'https://www.balenciaga.com' },
+];
