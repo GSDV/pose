f23d860f5f597cef0948dd7da003d24c643d2c7c
GSDV <sdvgab@gmail.com>
2025-03-24 19:38:23 -0500

    Capture screen both parts

--- app/(tabs)/(create)/capture/index.tsx
+++ app/(tabs)/(create)/capture/index.tsx
@@ -1,7 +1,33 @@
+import { useCallback, useState } from 'react';
+
+import { useRouter } from 'expo-router';
+
 import CapturePicture from '@screens/post/Create/CapturePicture';
+import CropPicture from '@screens/post/Create/CropImage';
 
 
 
 export default function Index() {
-    return <CapturePicture />;
+    const router = useRouter();
+
+    const [pictureUri, setPictureUri] = useState<string>('');
+
+
+    const setUri = useCallback((uri: string) => {
+        setPictureUri(uri);
+    }, []);
+
+    const onCropComplete = useCallback((uri: string) => {
+        router.push({
+            pathname: '/(tabs)/(create)/review',
+            params: { pictureUri: uri }
+        });
+    }, []);
+
+
+    return ((pictureUri == '') ?
+            <CapturePicture setUri={setUri} />
+        :
+            <CropPicture imageUri={pictureUri} setUri={setUri} onCropComplete={onCropComplete} />
+    );
 }
