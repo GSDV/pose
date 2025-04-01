67621b4b4012532aaf20f7647ab1f957b4a519fc
GSDV <sdvgab@gmail.com>
2025-04-01 16:14:20 -0500

    More types

--- src/util/types.ts
+++ src/util/types.ts
@@ -9,3 +9,39 @@ export interface ReviewDot {
     yPercent: number;
     color: string;
 }
+
+export interface Dot {
+    id: string;
+    title: string;
+    price: string;
+    currency: string;
+    imageUrl: string;
+    url: string;
+    xPercent: number;
+    yPercent: number;
+    color: string;
+    brand: string;
+}
+
+
+
+export interface RedactedUser {
+    id: string;
+    username: string;
+    displayName: string;
+    pfpKey: string;
+    bio: string;
+    followerCount: number;
+    followingCount: number;
+    verified: boolean;
+}
+
+
+
+export interface Post {
+    id: string;
+    author: RedactedUser;
+    caption: string;
+    image: string;
+    dots: Dot[];
+}
